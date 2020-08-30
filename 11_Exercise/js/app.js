1ff471691a6bff1cf930fadf8568ef19c724bf85
unknown <vivekbindal1209@gmail.com>
2020-08-31 01:07:19 +0530

    Calculator

---
+++ 11_Exercise/js/app.js
@@ -0,0 +1,48 @@
+  function button1() {
+    document.getElementById("myBtn").value += "1";    
+  }
+  function button2() {
+    document.getElementById("myBtn").value += "2";    
+  }
+    function button3() {
+    document.getElementById("myBtn").value += "3";    
+  }
+  function button4() {
+    document.getElementById("myBtn").value += "4";    
+  }
+  function button5() {
+    document.getElementById("myBtn").value += "5";    
+  }
+  function button6() {
+    document.getElementById("myBtn").value += "6";    
+  }
+    function button7() {
+    document.getElementById("myBtn").value += "7";    
+  }
+  function button8() {
+    document.getElementById("myBtn").value += "8";    
+  }
+  function button9() {
+    document.getElementById("myBtn").value += "9";    
+  }
+  function button0() {
+    document.getElementById("myBtn").value += "0";    
+  }
+  function buttonSum() {
+    document.getElementById("myBtn").value += "+";    
+  }
+    function buttonSub() {
+    document.getElementById("myBtn").value += "-";    
+  }
+  function buttonMulti() {
+    document.getElementById("myBtn").value += "*";    
+  }
+  function buttonDivide() {
+    document.getElementById("myBtn").value += "/";    
+  }
+  function buttonResult() {
+    document.getElementById("myBtn").value = eval(document.getElementById("myBtn").value);    
+  }
+  function buttonReset() {
+    document.getElementById("myBtn").value = "";    
+  }
